0fff39215097e8d42512856108c6039f761d799a
ragnhibs <r.b.skeie@cicero.oslo.no>
2025-06-23 10:19:36 +0200

    added check if variable exist

--- specify_output.py
+++ specify_output.py
@@ -3,7 +3,8 @@ outputpath = '/div/no-backup/users/ragnhibs/HYway/OsloCTM3output/'
 
 #Experiment and simulation information. To be used in the filename
 table_id = 'monthly'
-model_id = 'OsloCTM3-vtest2'
+model_id = 'OsloCTM3v1.2'
+#model_id = 'OsloCTM3v1.1'
 experiment_id = 'transient2010s'
 project_id = 'hyway'
 member_id = 'r1'
@@ -14,9 +15,23 @@ history_text = 'OsloCTM3 simulations for HYway, contact: r.b.skeie@cicero.oslo.n
 #Raw model output information
 #filepath = filepath +scen+'/'+yr+ '/'
 
-filepath =  '/div/qbo/users/srkri/'
-scen = 'VOC-OsloCTM3/CTM3_hyway_test2010.070425.72834'
+#filepath =  '/div/qbo/users/srkri/'
+#scen = 'VOC-OsloCTM3/CTM3_hyway_test2010.070425.72834'
+#yr = ''
+
+
+if model_id == 'OsloCTM3v1.1':
+    filepath = '/div/qbo/users/ragnhibs/HYway/CTM3results_wovocs/'
+elif model_id == 'OsloCTM3v1.2':
+    filepath = '/div/qbo/users/ragnhibs/HYway/CTM3results/'   
+
+
+scen = '2015'
 yr = ''
 
+#
+#scen = '2010'
+#yr = ''
+
 #List of meteorological year.
-metyear_list = [2010]
+metyear_list = [2015]
