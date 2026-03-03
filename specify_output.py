42ee976423e56c236c8e596b93d915f2207a651b
ragnhibs <r.b.skeie@cicero.oslo.no>
2026-03-03 09:00:38 +0100

    setup for 1850 runs

--- specify_output.py
+++ specify_output.py
@@ -1,18 +1,15 @@
 #Specify outputpath
-outputpath = '/div/no-backup/users/ragnhibs/HYway/OsloCTM3output/SIM2_v2/'
+outputpath = '/div/no-backup/users/ragnhibs/HYway/OsloCTM3output/SIM4/'
 
 #Experiment and simulation information. To be used in the filename
 table_id = 'monthly'
 model_id = 'OsloCTM3v1-2'
 
-#experiment_id = 'cntr'
-#experiment_id = 'h2pert'
-experiment_id = 'ch4pert'
-
-#experiment_id = 'ch3ohpert'
+experiment_id = 'cntr1850'
+#experiment_id = 'h2antr1850'
 
 project_id = 'hyway'
-member_id = 'r2'
+member_id = 'r1'
 
 #History text to be added to the file
 history_text = 'OsloCTM3 simulations for HYway, contact: r.b.skeie@cicero.oslo.no'
@@ -21,17 +18,13 @@ history_text = 'OsloCTM3 simulations for HYway, contact: r.b.skeie@cicero.oslo.n
 #year_use = 2019 #2013
 
 
+filepath = '/div/qbo/users/ragnhibs/HYway/CTM3results_sim4/'
 
-
-filepath = '/div/qbo/users/ragnhibs/HYway/CTM3results_sim2_v2/'
-
-scen_list = {'cntr':'CNTR',
-             'h2pert':'PERTH2',
-             'ch4pert':'PERTCH4',
-             'ch3ohpert':'PERTCH3OH'}
+scen_list = {'cntr1850':'cntr1850',
+             'h2antr1850':'h2antr1850'}
     
 scen = scen_list[experiment_id]
-yr = 'YR7'
+yr = 'YR1'
 
 yrstart = 2019 + int(yr[2:])*2-1
 print(yrstart)
